06d04b5ce953a54876aeef00b3e34ab72ee221e7
mgpnoahp <mgpnoahp@gmail.com>
2023-06-05 08:58:47 -0700

    Added Question Choice and Submission Models. And Added Exam to Course Details

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -101,10 +101,13 @@ class Enrollment(models.Model):
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-#class Question(models.Model):
+class Question(models.Model):
     # Foreign key to lesson
+    course = models.ForeignKey(Course , on_delete=models.CASCADE)
     # question text
+    content = models.CharField(max_length=256)
     # question grade/mark
+    points_worth = models.FloatField(default=1.0)
 
     # <HINT> A sample model method to calculate if learner get the score of the question
     #def is_get_score(self, selected_ids):
@@ -122,13 +125,19 @@ class Enrollment(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
+class Choice(models.Model):
+    question = models.ForeignKey(Question , on_delete=models.CASCADE)
+    content = models.CharField(max_length=32)
+    is_correct = models.BooleanField(default=False)
+
+
+
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    chocies = models.ManyToManyField(Choice)
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    chocies = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
